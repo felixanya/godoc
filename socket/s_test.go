d2f4263e45638cee805d320ccb46898f9c7bcded
felix <sgugarmanman@163.com>
2015-02-07 11:00:26 +0800

    Create s_test.go

---
+++ socket/s_test.go
@@ -0,0 +1,15 @@
+// Author: Felix
+// Email:  sugarmanman@163.com
+// 测试文档
+
+package socket_test
+
+import (
+	"fmt"
+)
+
+// 这个函数命名为ExampleSocket1（）
+// 这样一来godoc知道关联与实例类型它。
+func ExampleSocket1() {
+	   fmt.Println("test")
+}
